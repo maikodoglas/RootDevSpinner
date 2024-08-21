45802600193ac248db07617a765c5da3e43fd49c
maikodoglas <100614116+maikodoglas@users.noreply.github.com>
2024-08-21 17:14:05 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,291 +1,197 @@
 $(function () {
-  $leftWheel = $(".slot-wheel-left");
-  buildSlotWheels($leftWheel);
+    $leftWheel = $(".slot-wheel-left");
+	preloadSongs();
+    buildSlotWheels($leftWheel);
 });
-
-var listaCores = [
-  "#FEFFBF",
-  "#D1FFBA",
-  "#B8CBFF",
-  "#FFBDEB",
-  "#FFC08B",
-  "#FFFBA2",
-  "#D2FFBC",
-  "#C2F1FF",
-  "#DF8B76",
-  "#DDA576",
-  "#D496FF",
-  "#DFC575",
-  "#DFDA75",
-  "#CEDF74",
-  "#9CC2CB",
-  "#1ABB9C",
-  "#3498DB",
-  "#F7996E",
-  "#AE9CD6",
-  "#FFAEAE",
-  "#8BB9D4",
-  "#A6CFA1",
-  "#DAD6A1",
-  "#E1B298",
-  "#E59CA0",
-  "#D3FFCE",
-  "#7FFFD4",
-  "#F08080",
-  "#FFF68F",
-  "#66CDAA",
-  "#BADA55",
-  "#C0D6E4",
-];
 
 const title = "🎉🎊 PIÃO DO ROOTDEV 🎊🎉";
 const participants = [
-  "Alex Dalzoto",
-  "André Júnior",
-  "Bruno Molardi",
-  "Fabrício Sonego",
-  "Gustavo Sachet",
-  "Gabriel Marcon",
-  "Lucas Rech",
-  "Luis Silva",
-  "Maickel Douglas",
-  "Marcos Garbin",
-  "Ricardo Guimarães",
+    "Alex",
+    "André",
+    "Bruno",
+    "Fabrício",
+    "Gabriel",
+    "Gustavo",
+    "Lucas",
+    "Luis",
+    "Maickel",
+    "Marcos"
 ];
-let itemsListCount = 0;
 
-function buildSlotItem(imgURL) {
-  return $(
-    '<li class="item" style="background-color: ' +
-      listaCores[randomItemIndex(listaCores.length - 1)] +
-      '"><div style="padding-top: 245px; font-size: 100px">' +
-      imgURL +
-      "</div></li>"
-  );
+var songPiao = new Audio();
+var songPiaoRemix = new Audio();
+var songPiaoExtended = new Audio();
+var songPiaoDenovo = new Audio();
+var songPiaoStrikesBack = new Audio();
+var songPiaoGalvao = new Audio();
+var songPiaoUltimo = new Audio();
+var songPiaoHero = new Audio();
+var songSilvioTriggered = new Audio();
+var songPiaoStorm = new Audio();
+
+var spinParametersList = [
+    [43000, "easeInOutQuart", 12000, songPiao],
+    [42000, "easeOutQuad", 0, songPiaoRemix],
+    [153000, "easeOutBack", 0, songPiaoExtended],
+    [42000, "easeInOutQuart", 0, songPiaoDenovo],
+    [49000, "easeInOutQuart", 0, songPiaoStrikesBack],
+    [36800, "easeInBounce", 0, songPiaoGalvao],
+    [38700, "easeOutBack", 0, songPiaoUltimo],
+    [48000, "easeInOutQuart", 0, songPiaoHero],
+    [11000, "easeInOutQuart", 8000, songSilvioTriggered],
+    [10000, "easeOutQuad", 0, songPiaoStorm]
+]
+
+let itemsListCount = 0;
+let lastSpinNumber = 0;
+let lastColor = '#FEFFBF';
+let itemsArray = [];
+
+function preloadSongs(){
+    songPiao.src = "piao.mp3";
+    songPiao.preload = "auto";
+    songPiao.load();
+	
+	songPiaoRemix.src = "piao.remix.mp3";
+    songPiaoRemix.preload = "auto";
+    songPiaoRemix.load();
+	
+	songPiaoExtended.src = "piao-extended.mp3";
+    songPiaoExtended.preload = "auto";
+    songPiaoExtended.load();
+	
+	songPiaoDenovo.src = "piao-denovo.mp3";
+    songPiaoDenovo.preload = "auto";
+    songPiaoDenovo.load();
+	
+	songPiaoStrikesBack.src = "piao-strikes-back.mp3";
+    songPiaoStrikesBack.preload = "auto";
+    songPiaoStrikesBack.load();
+	
+	songPiaoGalvao.src = "piao-galvao.mp3";
+    songPiaoGalvao.preload = "auto";
+    songPiaoGalvao.load();
+	
+	songPiaoUltimo.src = "piao-ultimo.mp3";
+    songPiaoUltimo.preload = "auto";
+    songPiaoUltimo.load();
+	
+	songPiaoHero.src = "piao-hero.mp3";
+    songPiaoHero.preload = "auto";
+    songPiaoHero.load();
+	
+	songSilvioTriggered.src = "silvio-triggered.mp3";
+    songSilvioTriggered.preload = "auto";
+    songSilvioTriggered.load();
+	
+	songPiaoStorm.src = "piao-storm.mp3";
+    songPiaoStorm.preload = "auto";
+    songPiaoStorm.load();
 }
 
 function buildSlotWheels($container) {
-  let itemsArray = [];
-  itemsArray.push(title);
-  const amount = 666;
-  for (let i = 0; i < amount; i++) {
-    for (let participant of participants) {
-      itemsArray.push(participant);
+
+    itemsArray.push(title);
+    const amount = 2800;
+
+    for (let i = 0; i < amount; i++) {
+        for (let participant of participants) {
+            itemsArray.push(participant);
+        }
     }
-  }
-  itemsListCount = itemsArray.length;
-  $items = itemsArray.map(buildSlotItem);
-  $container.append($items);
+
+    itemsListCount = itemsArray.length;
+    $items = itemsArray.map(buildSlotItem);
+    $container.append($items);
 }
 
-function randomItemIndex(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+function buildSlotItem(imgURL) {
+    return $(
+        '<li class="item" style="background-color: ' +
+        randomColor() +
+        '"><div style="padding-top: 245px; font-size: 100px">' +
+        imgURL +
+        "</div></li>"
+    );
 }
 
-var contador = 0;
+function randomColor(){
+	return "hsl(" + 360 * Math.random() + ',' +
+             (25 + 70 * Math.random()) + '%,' + 
+             (45 + 60 * Math.random()) + '%)'
+}
 
 function rodaRoda() {
-  spinStart();
-  if (contador == 0) {
-    contador = 1;
-    spin();
-  } else if (contador == 1) {
-    contador = 2;
-    spin2();
-  } else if (contador == 2) {
-    contador = 3;
-    spin3();
-  } else if (contador == 3) {
-    contador = 4;
-    spin4();
-  } else if (contador == 4) {
-    contador = 5;
-    spin5();
-  } else if (contador == 5) {
-    contador = 6;
-    spin6();
-  } else if (contador == 6) {
-    contador = 7;
-    spin7();
-  } else if (contador == 7) {
-    contador = 8;
-    spin8();
-  } else if (contador == 8) {
-    contador = 9;
-    spin9();
-  } else if (contador == 9) {
-    contador = 0;
-    spin10();
-  }
+    spinStart();
+
+    var randomSpinNumber = randomItemIndex(spinParametersList.length);
+
+    while (lastSpinNumber === randomSpinNumber)
+        randomSpinNumber = randomItemIndex(spinParametersList.length);
+
+    var spinParameters = spinParametersList[randomSpinNumber];
+
+    spin(spinParameters[0], spinParameters[1], spinParameters[2], spinParameters[3]);
+
+    lastSpinNumber = randomSpinNumber;
 }
 
 function spinStart() {
-  document.getElementById("btnRodaRoda").innerText = "Rodando, rodando...";
-  //document.getElementById("btnRodaRoda").classList.add("rainbow");
-  //document.getElementById("btnRodaRoda").classList.add("rainbow-bg");
+    togglePride();
+    document.getElementById("btnRodaRoda").innerText = "Rodando, rodando...";
+}
+
+function spin(spinDuration, easing, cooldown, song) {
+	song.play();
+	
+	leftWheelIndex = randomItemIndex(itemsListCount);
+	var sortedPlayer = itemsArray[leftWheelIndex];
+
+    console.log(sortedPlayer, leftWheelIndex);
+
+    $leftWheel.animate(
+        {
+            top: -leftWheelIndex * 600,
+        },
+        spinDuration,
+        easing,
+        () => spinEnd(cooldown)
+    );
+	
+	if (randomItemIndex(4) == 3){
+		console.log('easter egg madness');
+		var waitTime = Math.floor(spinDuration * 70 / 100);
+		
+		setTimeout(function () {
+			document.getElementById("imgRootDev").style.display = "block";
+		}, waitTime);
+		setTimeout(function () {
+			document.getElementById("imgRootDev").style.display = "none";
+		}, waitTime + 300);
+	}
 }
 
 function spinEnd(cooldownTime = 0) {
-  party.screen();
-  setTimeout(() => {
-    document.getElementById("btnRodaRoda").innerText = "Sortear";
-  }, cooldownTime);
-  // document.getElementById("btnRodaRoda").classList.remove("rainbow");
-  // document.getElementById("btnRodaRoda").classList.remove("rainbow-bg");
+    party.screen();
+    togglePride();
+    setTimeout(() => {
+        document.getElementById("btnRodaRoda").innerText = "Sortear";
+    }, cooldownTime);
 }
 
-function spin() {
-  var leftWheelIndex = randomItemIndex(itemsListCount - 1);
-  var snd = new Audio("piao.mp3");
-  snd.play();
-  $leftWheel.animate(
-    {
-      top: -leftWheelIndex * 600,
-    },
-    43000,
-    "easeInOutQuart",
-    () => spinEnd(12000)
-  );
+function togglePride() {
+    document.getElementById('pride').classList.toggle("pride");
+    document.getElementById('btnRodaRoda').classList.toggle("pride");
 }
 
-function spin2() {
-  var leftWheelIndex = randomItemIndex(itemsListCount - 1);
-  console.log(leftWheelIndex);
-  var snd = new Audio("piao.remix.mp3");
-  snd.play();
-  $leftWheel.animate(
-    {
-      top: -leftWheelIndex * 600,
-    },
-    42000,
-    "easeOutQuad",
-    () => spinEnd()
-  );
-}
-
-function spin3() {
-  var leftWheelIndex = randomItemIndex(itemsListCount - 1);
-  console.log(leftWheelIndex);
-  var snd = new Audio("piao-extended.mp3");
-  snd.play();
-  $leftWheel.animate(
-    {
-      top: -leftWheelIndex * 600,
-    },
-    153000,
-    "easeOutBack",
-    () => spinEnd()
-  );
-
-  setTimeout(function () {
-    document.getElementById("imgRootDev").style.display = "block";
-  }, 108700);
-  setTimeout(function () {
-    document.getElementById("imgRootDev").style.display = "none";
-  }, 109000);
-}
-
-function spin4() {
-  var leftWheelIndex = randomItemIndex(itemsListCount - 1);
-  console.log(leftWheelIndex);
-  var snd = new Audio("piao-denovo.mp3");
-  snd.play();
-  $leftWheel.animate(
-    {
-      top: -leftWheelIndex * 600,
-    },
-    42000,
-    "easeInOutQuart",
-    () => spinEnd()
-  );
-}
-
-function spin5() {
-  var leftWheelIndex = randomItemIndex(itemsListCount - 1);
-  console.log(leftWheelIndex);
-  var snd = new Audio("piao-strikes-back.mp3");
-  snd.play();
-  $leftWheel.animate(
-    {
-      top: -leftWheelIndex * 600,
-    },
-    49000,
-    "easeInOutQuart",
-    () => spinEnd()
-  );
-}
-
-function spin6() {
-  var leftWheelIndex = randomItemIndex(itemsListCount - 1);
-  console.log(leftWheelIndex);
-  var snd = new Audio("piao-galvao.mp3");
-  snd.play();
-  $leftWheel.animate(
-    {
-      top: -leftWheelIndex * 600,
-    },
-    36800,
-    "easeInBounce",
-    () => spinEnd()
-  );
-}
-
-function spin7() {
-  var leftWheelIndex = randomItemIndex(itemsListCount - 1);
-  console.log(leftWheelIndex);
-  var snd = new Audio("piao-ultimo.mp3");
-  snd.play();
-  $leftWheel.animate(
-    {
-      top: -leftWheelIndex * 600,
-    },
-    38700,
-    "easeOutBack",
-    () => spinEnd()
-  );
-}
-
-function spin8() {
-  var leftWheelIndex = randomItemIndex(itemsListCount - 1);
-  console.log(leftWheelIndex);
-  var snd = new Audio("piao-hero.mp3");
-  snd.play();
-  $leftWheel.animate(
-    {
-      top: -leftWheelIndex * 600,
-    },
-    48000,
-    "easeInOutQuart",
-    () => spinEnd()
-  );
-}
-
-function spin9() {
-  var leftWheelIndex = randomItemIndex(itemsListCount - 1);
-  console.log(leftWheelIndex);
-  var snd = new Audio("silvio-triggered.mp3");
-  snd.play();
-  $leftWheel.animate(
-    {
-      top: -leftWheelIndex * 600,
-    },
-    11000,
-    "easeInOutQuart",
-    () => spinEnd(8000)
-  );
-}
-
-function spin10() {
-  var leftWheelIndex = randomItemIndex(itemsListCount - 1);
-  console.log(leftWheelIndex);
-  var snd = new Audio("piao-storm.mp3");
-  snd.play();
-  $leftWheel.animate(
-    {
-      top: -leftWheelIndex * 600,
-    },
-    10000,
-    "easeOutQuad",
-    () => spinEnd()
-  );
+function randomItemIndex(max) {
+    if (window.crypto && window.crypto.getRandomValues) {
+        var randomBuffer = new Uint32Array(1);
+        window.crypto.getRandomValues(randomBuffer);
+        let num = randomBuffer[0] % max;
+		return num;
+    } else {
+        alert('navegador not compatible');
+		return 0;
+    }
 }
